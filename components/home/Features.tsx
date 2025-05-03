cb9f6042d96da0c40384e516b5d0dcf111e6c003
DanieII <danielbogdanov1748@gmail.com>
2025-05-03 21:25:47 +0300

    Update README with instructions

--- components/home/Features.tsx
+++ components/home/Features.tsx
@@ -1,87 +1,87 @@
 export default function Features() {
   return (
-    <div className="bg-base-200" id="features">
-      <div className="container mx-auto px-8 py-20">
-        <div className="flex flex-col items-center space-y-8">
-          <h2 className="text-center text-4xl font-bold">Features</h2>
-          <div className="mx-auto flex max-w-xl flex-col gap-8">
-            <div className="flex items-center gap-4">
-              <div className="bg-primary self-start rounded-full p-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="text-primary-content size-6"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M14.615 1.595a.75.75 0 0 1 .359.852L12.982 9.75h7.268a.75.75 0 0 1 .548 1.262l-10.5 11.25a.75.75 0 0 1-1.272-.71l1.992-7.302H3.75a.75.75 0 0 1-.548-1.262l10.5-11.25a.75.75 0 0 1 .913-.143Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-              <div className="space-y-2">
-                <p className="text-xl font-semibold">
-                  Lorem ipsum dolor sit amet
-                </p>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui,
-                  natus. Ducimus distinctio eum esse voluptates quod at quis
-                  fugit.
-                </p>
-              </div>
+    <div className="container mx-auto px-8 py-20" id="features">
+      <div className="flex flex-col items-center space-y-8">
+        <h2 className="text-center text-4xl font-bold tracking-tight">
+          Features
+        </h2>
+        <div className="mx-auto flex max-w-xl flex-col gap-8">
+          <div className="flex items-center gap-4">
+            <div className="bg-primary self-start rounded-full p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="text-primary-content size-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M14.615 1.595a.75.75 0 0 1 .359.852L12.982 9.75h7.268a.75.75 0 0 1 .548 1.262l-10.5 11.25a.75.75 0 0 1-1.272-.71l1.992-7.302H3.75a.75.75 0 0 1-.548-1.262l10.5-11.25a.75.75 0 0 1 .913-.143Z"
+                  clipRule="evenodd"
+                />
+              </svg>
             </div>
-            <div className="flex items-center gap-4">
-              <div className="bg-secondary self-start rounded-full p-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="text-secondary-content size-6"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M14.615 1.595a.75.75 0 0 1 .359.852L12.982 9.75h7.268a.75.75 0 0 1 .548 1.262l-10.5 11.25a.75.75 0 0 1-1.272-.71l1.992-7.302H3.75a.75.75 0 0 1-.548-1.262l10.5-11.25a.75.75 0 0 1 .913-.143Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-              <div className="space-y-2">
-                <p className="text-xl font-semibold">
-                  Lorem ipsum dolor sit amet
-                </p>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui,
-                  natus. Ducimus distinctio eum esse voluptates quod at quis
-                  fugit.
-                </p>
-              </div>
+            <div className="space-y-2">
+              <p className="text-xl font-semibold">
+                Lorem ipsum dolor sit amet
+              </p>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui,
+                natus. Ducimus distinctio eum esse voluptates quod at quis
+                fugit.
+              </p>
             </div>
-            <div className="flex items-center gap-4">
-              <div className="bg-accent self-start rounded-full p-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="text-accent-content size-6"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M14.615 1.595a.75.75 0 0 1 .359.852L12.982 9.75h7.268a.75.75 0 0 1 .548 1.262l-10.5 11.25a.75.75 0 0 1-1.272-.71l1.992-7.302H3.75a.75.75 0 0 1-.548-1.262l10.5-11.25a.75.75 0 0 1 .913-.143Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-              <div className="space-y-2">
-                <p className="text-xl font-semibold">
-                  Lorem ipsum dolor sit amet
-                </p>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui,
-                  natus. Ducimus distinctio eum esse voluptates quod at quis
-                  fugit.
-                </p>
-              </div>
+          </div>
+          <div className="flex items-center gap-4">
+            <div className="bg-secondary self-start rounded-full p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="text-secondary-content size-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M14.615 1.595a.75.75 0 0 1 .359.852L12.982 9.75h7.268a.75.75 0 0 1 .548 1.262l-10.5 11.25a.75.75 0 0 1-1.272-.71l1.992-7.302H3.75a.75.75 0 0 1-.548-1.262l10.5-11.25a.75.75 0 0 1 .913-.143Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="space-y-2">
+              <p className="text-xl font-semibold">
+                Lorem ipsum dolor sit amet
+              </p>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui,
+                natus. Ducimus distinctio eum esse voluptates quod at quis
+                fugit.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-4">
+            <div className="bg-accent self-start rounded-full p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="text-accent-content size-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M14.615 1.595a.75.75 0 0 1 .359.852L12.982 9.75h7.268a.75.75 0 0 1 .548 1.262l-10.5 11.25a.75.75 0 0 1-1.272-.71l1.992-7.302H3.75a.75.75 0 0 1-.548-1.262l10.5-11.25a.75.75 0 0 1 .913-.143Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="space-y-2">
+              <p className="text-xl font-semibold">
+                Lorem ipsum dolor sit amet
+              </p>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui,
+                natus. Ducimus distinctio eum esse voluptates quod at quis
+                fugit.
+              </p>
             </div>
           </div>
         </div>
--- components/home/Pricing.tsx
+++ components/home/Pricing.tsx
@@ -6,16 +6,16 @@ const plans = [
       process.env.NODE_ENV === "development"
         ? "price_************************"
         : "price_************************",
-    price: 1,
-    title: "Lorem",
+    price: 10,
+    title: "Monthly",
   },
   {
     priceId:
       process.env.NODE_ENV === "development"
         ? "price_************************"
         : "price_************************",
-    price: 1,
-    title: "Ipsum",
+    price: 5,
+    title: "Yearly",
   },
 ];
 
@@ -23,19 +23,21 @@ export default function Pricing() {
   return (
     <div className="container mx-auto px-8 py-20" id="pricing">
       <div className="space-y-8">
-        <h2 className="text-center text-4xl font-bold">Pricing</h2>
+        <h2 className="text-center text-4xl font-bold tracking-tight">
+          Pricing
+        </h2>
         <div className="flex flex-col justify-center gap-8 lg:flex-row">
           {plans.map((plan) => (
             <div
-              className="card bg-base-100 w-full shadow-sm lg:w-96"
+              className={`card bg-base-100 w-full shadow-sm lg:w-96 ${plan.title === "Yearly" && "border-primary border-2"}`}
               key={plan.title}
             >
               <div className="card-body">
                 <div className="flex justify-between">
                   <h2 className="text-3xl font-bold">{plan.title}</h2>
-                  <span className="text-xl">$29/mo</span>
+                  <span className="text-xl">${plan.price}/mo</span>
                 </div>
-                <ul className="mt-6 flex flex-col gap-2 text-xs">
+                <ul className="my-6 flex flex-col gap-2 text-xs">
                   <li>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -104,8 +106,27 @@ export default function Pricing() {
                     </svg>
                     <span>AI-driven image enhancements</span>
                   </li>
+                  {plan.title === "Yearly" && (
+                    <li>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="text-success me-2 inline-block size-4"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M5 13l4 4L19 7"
+                        />
+                      </svg>
+                      <span className="text-success">Save $60 (50%)</span>
+                    </li>
+                  )}
                 </ul>
-                <div className="mt-6">
+                <div className="mt-auto">
                   <SubscribeButton priceId={plan.priceId} />
                 </div>
               </div>

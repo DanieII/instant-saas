497853492dad4f0b3770f03d1845c829e5526ebe
DanieII <danielbogdanov1748@gmail.com>
2025-05-19 09:42:23 +0300

    Remove unnecessary subscription policies in supabase

---
+++ supabase/migrations/20250519063813_subscriptions.sql
@@ -0,0 +1,7 @@
+drop policy "Enable delete for users based on user_id" on "public"."subscriptions";
+
+drop policy "Enable insert for users based on user_id" on "public"."subscriptions";
+
+drop policy "Update policy: Enable update for users based on user_id" on "public"."subscriptions";
+
+
